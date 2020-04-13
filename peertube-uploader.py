4628ec771f54b904daf89d77423fbf29f9651572
Dmitri Plakhov <d.pl@mail.ru>
2020-04-13 22:24:52 +0300

    refactor config and redis

--- peertube-uploader.py
+++ peertube-uploader.py
@@ -6,50 +6,27 @@ import sys
 import logging
 from mimetypes import guess_type
 import requests
-from config import LOG_FILE_PATH, PEERTUBE_CHANNEL, PEERTUBE_CLIENT_ID, \
-        PEERTUBE_CLIENT_SECRET, PEERTUBE_TOKEN, PEERTUBE_ENDPOINT, \
-        PEERTUBE_USER, PEERTUBE_PASSWORD
+from tokenstore import get_token
+from config import Config as c
 
 
 logging.basicConfig(
-        filename=LOG_FILE_PATH,
-        format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
-        level=logging.INFO
-        )
+    filename=c.LOG_FILE_PATH,
+    format=u'[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+    level=logging.INFO
+    )
 
 videodir = sys.argv[1]
-
-auth_data = {'client_id': PEERTUBE_CLIENT_ID,
-             'client_secret': PEERTUBE_CLIENT_SECRET,
-             'grant_type': 'password',
-             'response_type': 'code',
-             'username': PEERTUBE_USER,
-             'password': PEERTUBE_PASSWORD
-             }
-
-if PEERTUBE_TOKEN == 'access_token':
-    try:
-        auth_result = requests.post(
-            '{0}{1}'.format(PEERTUBE_ENDPOINT, '/api/v1/users/token'),
-            data=auth_data
-        )
-        access_token = (auth_result.json()['access_token'])
-        logging.info('{0} {1}'.format("Got new token:", access_token))
-    except:
-        logging.error(auth_result.text)
-        sys.exit(1)
-else:
-    access_token = PEERTUBE_TOKEN
 
 for videofile in os.listdir(videodir):
     if videofile.endswith(".mp4"):
         file_path = os.path.join(videodir, videofile)
         file_name = os.path.basename(file_path)
         file_mime_type = guess_type(file_path)[0]
-        logging.info('{0}{1}'.format(u'Accept file for uploading: ', file_name))
+        logging.info("Accept file for uploading: " % file_name)
 
         channelId = requests.get(
-            '{0}{1}/{2}'.format(PEERTUBE_ENDPOINT, '/api/v1/video-channels', PEERTUBE_CHANNEL)
+            '{0}{1}/{2}'.format(c.PEERTUBE_ENDPOINT, '/api/v1/video-channels', c.PEERTUBE_CHANNEL)
             ).json()['id']
 
         upload_data = {'channelId': channelId,
@@ -58,16 +35,19 @@ for videofile in os.listdir(videodir):
                        'name': file_name
                        }
 
-        upload_headers = {'Authorization': 'Bearer {0}'.format(PEERTUBE_TOKEN)}
+        access_token = get_token()
+        upload_headers = {'Authorization': 'Bearer {0}'.format(access_token)}
 
         with open(file_path, 'rb') as f:
-            upload_result = requests.post('{0}{1}'.format(PEERTUBE_ENDPOINT, '/api/v1/videos/upload'),
-                                          headers=upload_headers,
-                                          data=upload_data,
-                                          files={"videofile": (file_name, f, file_mime_type)})
+            upload_result = requests.post(
+                '{0}{1}'.format(c.PEERTUBE_ENDPOINT, '/api/v1/videos/upload'),
+                headers=upload_headers,
+                data=upload_data,
+                files={"videofile": (file_name, f, file_mime_type)}
+                )
             try:
                 video_uuid = upload_result.json()['video']['uuid']
-                logging.info('{0}{1}/{2}'.format(PEERTUBE_ENDPOINT, '/videos/watch', video_uuid))
+                logging.info("%s%s/%s" % (c.PEERTUBE_ENDPOINT, '/videos/watch', video_uuid))
             except:
                 logging.info(upload_result.text)
                 sys.exit(1)
--- tokenstore.py
+++ tokenstore.py
@@ -1,17 +1,68 @@
-import redis
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import sys
 from datetime import timedelta
-from config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
+import logging
+import requests
+import redis
+from config import Config as c
 
 
 def get_redis():
     r = redis.Redis(
-        host=REDIS_HOST,
-        port=REDIS_PORT,
+        host=c.REDIS_HOST,
+        port=c.REDIS_PORT,
         db=1,
-        password=REDIS_PASSWORD
+        password=c.REDIS_PASSWORD
     )
     return r
 
-def check_token(token):
-    r = get_redis()
 
+def get_token():
+    r = get_redis()
+    if r.ttl("pttoken") > 1000:
+        token = r.get("pttoken")
+        logging.info('Got token locally: %s' % token)
+    else:
+        token = get_token_from_pt()
+    return token
+
+
+def set_token(token, seconds=10000):
+    r = get_redis()
+    r.setex(
+        "pttoken",
+        timedelta(seconds=seconds),
+        value=token
+    )
+
+
+def get_token_from_pt():
+    auth_data = {'client_id': c.PEERTUBE_CLIENT_ID,
+                 'client_secret': c.PEERTUBE_CLIENT_SECRET,
+                 'grant_type': 'password',
+                 'response_type': 'code',
+                 'username': c.PEERTUBE_USER,
+                 'password': c.PEERTUBE_PASSWORD
+                 }
+
+    if c.PEERTUBE_TOKEN == 'access_token':
+        try:
+            auth_result = requests.post(
+                '{0}{1}'.format(c.PEERTUBE_ENDPOINT, '/api/v1/users/token'),
+                data=auth_data
+                )
+            access_token = (auth_result.json()['access_token'])
+            seconds = (auth_result.json()['expires_in'])
+            set_token(access_token, seconds)
+            logging.info('Got new token from peertube: %s' % access_token)
+        except:
+            logging.error(auth_result.text)
+            sys.exit(1)
+    else:
+        access_token = c.PEERTUBE_TOKEN
+
+    set_token(access_token)
+    return access_token
